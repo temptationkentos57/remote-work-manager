c2585edebd4b7af6dedf01cb7c37fb86ecb287ff
temptationkentos57 <kreetlannoo@hotmail.com>
2025-08-31 17:06:53 +0200

    Update welcome message to English for broader audience

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Chào mừng đến với Remote Work Manager!');
+  res.send('Welcome to the Remote Work Manager!');
 });
 
 app.listen(port, () => {
-  console.log(`Server đang chạy trên http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
