1f6d5f71722147bc5e9d63885d959676e643f9f0
temptationkentos57 <kreetlannoo@hotmail.com>
2025-08-31 17:07:42 +0200

    Add middleware to parse JSON request bodies

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Welcome to the Remote Work Manager!');
 });
